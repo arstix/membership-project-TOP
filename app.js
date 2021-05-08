77d981e2032e7d6f426de733b1ed788cb7af1f21
arstix <runjie30@gmail.com>
2021-05-08 13:28:45 +0200

    fourth commit

--- app.js
+++ app.js
@@ -247,4 +247,5 @@ app.post('/sign-up', check('password').exists(),
 
 
 app.use(express.urlencoded({ extended: false }));
-app.listen(3000, () => console.log("app listening on port 3000!"));
+app.listen(process.env.PORT || 5000)
+, () => console.log("app listening on port 3000!"));
